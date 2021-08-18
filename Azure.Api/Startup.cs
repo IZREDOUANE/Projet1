661b57ca0b01ff9bb6fc6273610a36ea8d209a40
r.izeghouine <r.izeghouine@R--IZEGHIOUINE-L>
2021-08-18 12:00:31 +0200

    modifi

--- Azure.Api/Startup.cs
+++ Azure.Api/Startup.cs
@@ -39,7 +39,7 @@ namespace Azure.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+     
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IConfiguration>(Configuration);
